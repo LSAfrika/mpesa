f09cb2c3c6166539e02ba7ec04fc485791a57963
LSAfrika <lstudiosafrika@gmail.com>
2022-07-22 14:18:31 +0300

    added stk psuh

--- index.js
+++ index.js
@@ -23,13 +23,161 @@ app.get('/accesstoken',authsafnode,(req,res)=>{
 
 })
 
-function time(){
+// day()
+
+// makejson()
+
+app.get('/stkpush',authsafnode,async(req,res)=>{
+    try {
+   
+        const auth = 'Bearer '+req.body.access_token
+         const timestamp = Timestamp()
+
+        //  to generate pass word we join the SHORTCODE + PASSKEY + TIMESTAMP into base 64 Buffer.from(shortcode+passkey+timestamp).toString('base64')
+         const passwordbuffer= Buffer.from('174379'+'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919'+timestamp).toString('base64')
+        //  console.log('buffer password: ',passwordbuffer);
+         // the pass key and time stamp were attained from decoding the encoded password
+         const testpassword= Buffer.from('174379'+'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919'+'20160216165627').toString('base64')
+        
+        //  console.log('password: ',testpassword);
+        const data={
+            BusinessShortCode:"174379",    
+            Password: "MTc0Mzc5YmZiMjc5ZjlhYTliZGJjZjE1OGU5N2RkNzFhNDY3Y2QyZTBjODkzMDU5YjEwZjc4ZTZiNzJhZGExZWQyYzkxOTIwMTYwMjE2MTY1NjI3",    
+          Timestamp:"20160216165627",    
+          TransactionType: "CustomerPayBillOnline",    
+            Amount:"1",    
+           PartyA:"254720849101",    
+            PartyB:"174379",    
+          PhoneNumber:"254720849101",    
+          CallBackURL:"http://197.248.90.146:2000/callback",    
+          AccountReference:"Test",    
+          TransactionDesc:"Test sim tool kit"
+        }
+
+        const payload = JSON.stringify(data)
+
+       // safcom test line 254708374149
+
+        const stkresult = await axios.post(stkpushurl,
+            { 
+                    
+                "BusinessShortCode": "174379",
+                "Password": `${passwordbuffer}`,
+                "Timestamp": `${timestamp}`,
+                "TransactionType": "CustomerPayBillOnline",
+                "Amount": "1",
+                "PartyA": "254742226392",
+                "PartyB": "174379",
+                "PhoneNumber": "254742226392",
+                "CallBackURL": "https://mydomain.com/pat",
+                "AccountReference": "Test",
+                "TransactionDesc": "Test"
+                 
+
+         },{headers:{'Authorization':auth}})
+      //  console.log('stk result:',stkresult.data);
+
+        res.send({message:stkresult.data})
+        
+    } catch (error) {
+
+      //  console.log('error encounterd stk push: ',error.message);
+        res.send({message:error.message,err:error})
+        
+    }
+
+})
+
+
+app.post('/callback',(req,res)=>{
+    console.log(req.body);
+})
+
+
+
+app.post('/validation',(req,res)=>{
+    console.log('validation: ',req.body);
+    res.status(200).send({message:'ok'})
+
+})
+
+app.post('/confirmation',(req,res)=>{
+
+    console.log('confirmation: ',req.body);
+    res.status(200).send({message:'ok'})
+    
+})
+
+app.post('/registerurls',registerurl,(req,res)=>{
+    console.log('body: ',req.body);
+    res.send({message:'urls registered'})
+})
+
+
+
+
+async function authsafnode(req,res,next){
+
+    // get basic auth by conination of  consumer key:consumer secret to base 64
+    try {
+        const consumerkey='pRYr6zjq0i8L1zzwQDMLpZB3yPCkhMsc'
+        const consumersecret='Rf22BfZog0qQGlV9'
+        const keytoencode=consumerkey+':'+consumersecret
+        const auth = Buffer.from(keytoencode).toString('base64')
+        // console.log("auth token: ",auth);
+
+        const result = await axios.get('https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials',
+        {
+            headers:{'Authorization':`Basic ${auth}`}
+        })
+
+        // console.log(result.data.access_token);
+        req.body.access_token=result.data.access_token
+
+        next()
+
+        
+    } catch (error) {
+        
+        console.log('error caught: ',error.message);
+        res.status(500).send({message:'server error',
+    err:error.message})
+    }
+
+}
+
+function registerurl(req,res,next){
+
+     unirest('POST', 'https://sandbox.safaricom.co.ke/mpesa/c2b/v1/registerurl')
+.headers({
+	'Content-Type': 'application/json',
+	'Authorization': 'Bearer 3OAFzy0qsEdjGIzowANdJIPMEAVf'
+})
+.send(JSON.stringify({
+    "ShortCode": 600986,
+    "ResponseType": "Completed",
+    "ConfirmationURL": "https://102.167.247.189:2000/confirmation",
+    "ValidationURL": "https://102.167.247.189:2000/validation",
+  }))
+.end(Fres => {
+	if (Fres.error) throw new Error(Fres.error);
+	console.log(Fres.raw_body);
+
+    req.body.urls =Fres.raw_body
+    next()
+})
+}
+
+
+function Timestamp(){
     const timestamp=new Date()
-    // const date=mapdate()
-    const date=timestamp.getFullYear()+""+mapmonth()+mapdate()+""+maphour()+""+timestamp.getMinutes()+""+mapseconds()
-    console.log(date);
+
+    const date=timestamp.getFullYear()+""+mapmonth()+mapdate()+""+maphour()+""+mapminutes()+""+mapseconds()
+
     return date 
 }
+
+
 function maphour(){
     const timestamp=new Date()
     let Returnedhour=''
@@ -83,7 +231,23 @@ function mapmonth(){
 
 }
 
-function mapseconds(){
+function mapminutes(){
+    const timestamp=new Date()
+    let returnedminutes=''
+    const minutes = timestamp.getMinutes()
+    if(minutes<10){
+        returnedminutes='0'+minutes
+      //  console.log('month:',month);
+        return returnedminutes
+
+    }else{
+
+        returnedminutes=''+minutes
+      //  console.log(returnedminutes);
+        return returnedminutes
+    }
+
+}function mapseconds(){
     const timestamp=new Date()
     let returnedseconds=''
     const seconds = timestamp.getSeconds()
@@ -100,115 +264,9 @@ function mapseconds(){
     }
 
 }      
-time()
-// day()
-
-app.get('/stkpush',authsafnode,async(req,res)=>{
-    try {
-   
-        const auth = 'Bearer '+req.body.access_token
-
-        const stkres = axios.post(stkpushurl,{ 
- 
-        },
-            
-            {
-                headers:{
-                    'Authorization':auth
-                }
-            }
-            
-            
-            
-            )
-        
-    } catch (error) {
-        
-    }
-
-})
 
 
 
-
-app.post('/validation',(req,res)=>{
-    console.log('validation: ',req.body);
-    res.status(200).send({message:'ok'})
-
-})
-
-app.post('/confirmation',(req,res)=>{
-
-    console.log('confirmation: ',req.body);
-    res.status(200).send({message:'ok'})
-    
-})
-
-app.post('/registerurls',registerurl,(req,res)=>{
-    console.log('body: ',req.body);
-    res.send({message:'urls registered'})
-})
-
-
-function authsafcom(req,res,next){
-    unirest('GET', 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials')
-    .headers({ 'Authorization': 'Basic cFJZcjZ6anEwaThMMXp6d1FETUxwWkIzeVBDa2hNc2M6UmYyMkJmWm9nMHFRR2xWOQ==' })
-    .send()
-    .end(lres => {
-        if (lres.error) throw new Error(lres.error);
-     //   console.log(lres.raw_body);
-
-     //  return res.send(lres.raw_body)
-
-       req.body.token=lres.raw_body
-       next();
-    });
-}
-
-async function authsafnode(req,res,next){
-    try {
-
-        const result = await axios.get('https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials',
-        {
-            headers:{'Authorization':'Basic cFJZcjZ6anEwaThMMXp6d1FETUxwWkIzeVBDa2hNc2M6UmYyMkJmWm9nMHFRR2xWOQ=='}
-        })
-
-        // console.log(result.data.access_token);
-        req.body.access_token=result.data.access_token
-
-        next()
-
-        
-    } catch (error) {
-        
-        console.log('error caught: ',error.message);
-        res.status(500).send({message:'server error',
-    err:error.message})
-    }
-
-}
-
-function registerurl(req,res,next){
-
-     unirest('POST', 'https://sandbox.safaricom.co.ke/mpesa/c2b/v1/registerurl')
-.headers({
-	'Content-Type': 'application/json',
-	'Authorization': 'Bearer 3OAFzy0qsEdjGIzowANdJIPMEAVf'
-})
-.send(JSON.stringify({
-    "ShortCode": 600986,
-    "ResponseType": "Completed",
-    "ConfirmationURL": "https://102.167.247.189:2000/confirmation",
-    "ValidationURL": "https://102.167.247.189:2000/validation",
-  }))
-.end(Fres => {
-	if (Fres.error) throw new Error(Fres.error);
-	console.log(Fres.raw_body);
-
-    req.body.urls =Fres.raw_body
-    next()
-})
-}
 
 //authsafcom()
 app.listen(2000,()=>{
